3e08070620c39425d7ae28b241faf2b808253cf5
musaprg <musaprg@users.noreply.github.com>
2018-10-27 16:00:41 +0900

    Change prov scripts and ignore share files

--- scripts/prov.sh
+++ scripts/prov.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
+
+cat <<HEREDOC > /etc/environment
+LANG=ja_JP.UTF-8
+LC_ALL=ja_JP.UTF-8
+HEREDOC
+
 yum update -y
+yum install -y wget unzip
 
